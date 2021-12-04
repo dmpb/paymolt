27caeb047c3842134b440ecab8b008c0ce1236f1
Daniel Ponce <51546545+dmpb@users.noreply.github.com>
2021-12-03 21:40:27 -0500

    Add test card to Payments/Create

--- resources/js/Pages/Payments/Create.vue
+++ resources/js/Pages/Payments/Create.vue
@@ -75,6 +75,31 @@
                             >
                                 {{ paymentLink.description }}
                             </p>
+
+                            <a
+                                href="https://docs.culqi.com/#/desarrollo/tarjetas"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <div
+                                    class="flex items-center mt-3 text-sm font-semibold text-indigo-700 "
+                                >
+                                    <div>Tarjetas de Prueba</div>
+
+                                    <div class="ml-1 text-indigo-500">
+                                        <svg
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                            class="w-4 h-4"
+                                        >
+                                            <path
+                                                fill-rule="evenodd"
+                                                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                                clip-rule="evenodd"
+                                            ></path>
+                                        </svg>
+                                    </div></div
+                            ></a>
                         </div>
                         <!-- Footer -->
                         <div class="hidden md:flex">
